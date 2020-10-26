7a1d7232032dde34d5d5cf9c2895825b0c824689
Oleksiy Lyulin <oleksiy.lyulin@selectoglobal.com>
2020-10-26 19:05:19 +0200

    FE: Implemented steps 1 - 11

--- backoffice/src/state/entities/vehicles.ts
+++ backoffice/src/state/entities/vehicles.ts
@@ -1,104 +1,138 @@
-import {
-    observable, action, computed, autorun, when,
-} from 'mobx'
-import { now } from 'mobx-utils'
+import { observable, action, computed, autorun, when } from "mobx";
+import { now } from "mobx-utils";
 
-import {moment, base64, axios} from 'utils/misc'
-import {AxiosInstance} from "axios";
+import { moment, base64, axios } from "utils/misc";
+import { AxiosInstance } from "axios";
 
 type Vehicle = {
-    id: string,
-    name: string,
-    vehicle: string,
-    location: number[],
-    online: boolean,
-}
+  id: string;
+  name: string;
+  vehicle: string;
+  location: number[];
+  online: boolean;
+};
 
 const vehicleTypes: any = {
-    truck: 'Camion',
-}
+  truck: "Camion",
+};
 
 export default class StateVehicles {
-    @observable list: Vehicle[] = []
+  @observable list: Vehicle[] = [];
 
-    @observable configuration: any
+  @observable configuration: any;
 
-    @observable selectedVehicleId: string | null = null
+  @observable selectedVehicleId: string | null = null;
 
-    api: AxiosInstance | null = null
+  api: AxiosInstance | null = null;
 
-    session: any = null
+  session: any = null;
 
-    constructor(session: any, configurationPromise: any) {
-        this.session = session
+  constructor(session: any, configurationPromise: any) {
+    this.session = session;
 
-        if (typeof window !== 'undefined') {
-            configurationPromise.then((configuration: any) => {
-                this.configuration = configuration
+    if (typeof window !== "undefined") {
+      configurationPromise.then((configuration: any) => {
+        this.configuration = configuration;
 
-                // Create API client
-                this.api = axios.create({
-                    baseURL: `${this.configuration.endpoint.host}`,
-                    timeout: 10000,
-                    headers: { 'Content-Type': 'application/json' },
-                    responseType: 'json',
-                })
+        // Create API client
+        this.api = axios.create({
+          baseURL: `${this.configuration.endpoint.host}`,
+          timeout: 10000,
+          headers: { "Content-Type": "application/json" },
+          responseType: "json",
+        });
 
-                autorun(() => {
-                    if (session.online && session.token) {
-                        this.updateVehicles(session).then(() => {})
-                    }
-                })
+        this.api.interceptors.request.use(function (config) {
+            const token = localStorage.getItem('token');
+            config.headers.Authorization =  token ? `Bearer ${token}` : '';
+            return config;
+          });
 
-                autorun(() => {
-                    now(1000)
-                    if (session.online && session.token) {
-                        this.updateVehicles(session).then(() => {})
-                    }
-                })
+        autorun(() => {
+          if (session.online && session.token) {
+            this.updateVehicles(session).then(() => {});
+          }
+        });
 
-                when(() => session.token, () => {
-                    this.updateVehicles(session).then(() => {})
-                })
-            })
-        }
+        autorun(() => {
+          now(1000);
+          if (session.online && session.token) {
+            this.updateVehicles(session).then(() => {});
+          }
+        });
+
+        when(
+          () => session.token,
+          () => {
+            this.updateVehicles(session).then(() => {});
+          }
+        );
+      });
     }
+  }
 
-    @action.bound async updateVehicles(session: any) {
-        // FIXME
+  @action.bound async updateVehicles(session: any) {
+    if (this.api) {
+      try {
+        // this.api.defaults.headers.common.Authorization = `Bearer ${session.token}`;
+        const responce = await this.api?.get("vehicles");
+        this.updateList(responce.data.vehicles);
+      } catch {}
     }
+  }
 
-    @action.bound updateList(vehicles: any[]) {
-        this.list = vehicles
-            .slice()
-            .sort((v1: Vehicle, v2: Vehicle) => v1.name.toLowerCase().localeCompare(v2.name.toLowerCase()))
-            .map((vehicle: any) => {
-                return {
-                    ...vehicle,
-                    vehicle: vehicleTypes[vehicle.vehicle] ? vehicleTypes[vehicle.vehicle] : 'Inconnu',
-                }
-            })
-    }
+  @action.bound async setOnline(id: string) {
+    try {
+      await this.api?.post(`vehicles/online/${id}`);
+      this.updateVehicles(this.session);
+    } catch {}
+  }
 
-    @action.bound selectVehicle(id: string) {
-        this.selectedVehicleId = id
-    }
+  @action.bound async setOffline(id: string) {
+    try {
+      await this.api?.post(`vehicles/offline/${id}`);
+      this.updateVehicles(this.session);
+    } catch {}
+  }
 
-    @computed get all() : Vehicle[] {
-        return this.list
-    }
+  @action.bound updateList(vehicles: any[]) {
+    this.list = vehicles
+      .slice()
+      .sort((v1: Vehicle, v2: Vehicle) =>
+        v1.name.toLowerCase().localeCompare(v2.name.toLowerCase())
+      )
+      .map((vehicle: any) => {
+        return {
+          ...vehicle,
+          vehicle: vehicleTypes[vehicle.vehicle]
+            ? vehicleTypes[vehicle.vehicle]
+            : "Inconnu",
+          updateTime: moment(vehicle.updateTime).format("DD/MM/YYYY-HH:mm:ss"),
+        };
+      });
+  }
 
-    @computed get online() : Vehicle[] {
-        return this.list.filter((vehicle: any) => vehicle.online)
-    }
+  @action.bound selectVehicle(id: string) {
+    this.selectedVehicleId = id;
+  }
 
-    @computed get selectedVehicle() : Vehicle | null {
-        if (this.selectedVehicleId) {
-            const vehicle = this.list.find((vehicle: any) => vehicle.id === this.selectedVehicleId)
-            if (vehicle) {
-                return vehicle
-            }
-        }
-        return null
+  @computed get all(): Vehicle[] {
+    return this.list;
+  }
+
+  @computed get online(): Vehicle[] {
+    return this.list.filter((vehicle: any) => vehicle.online);
+  }
+
+  @computed get selectedVehicle(): Vehicle | null {
+    if (this.selectedVehicleId) {
+      const vehicle = this.list.find(
+        (vehicle: any) => vehicle.id === this.selectedVehicleId
+      );
+      if (vehicle) {
+        return vehicle;
+      }
     }
+    return null;
+  }
 }
